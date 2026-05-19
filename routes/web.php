89b694239a7e2d700d38ef77626d56687ee477a7
khushaliSavaliya <khushalisavaliya28@gmail.com>
2026-05-19 21:36:58 +0530

    updated route on routes file

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landing');
-});
+    return view('home');
+})->name('home');
+
+// Future: contact form POST
+// Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
