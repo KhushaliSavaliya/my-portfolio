81c03088086b3ba69c4ba2d063f3fc6d0f9119ad
khushaliSavaliya <khushalisavaliya28@gmail.com>
2026-05-23 21:15:11 +0530

    removed contact route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
-
-// Future: contact form POST
-// Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
